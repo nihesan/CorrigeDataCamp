95bb01ed593f74b14266832422aa1bb3d8a068d4
nihesan <nihesan@alumni.uv.es>
2026-04-17 18:22:44 +0200

    Listado de carpetas de cada estudiante(archivos) y búsqueda de los archivos de puntos

--- RScriptTarea.R
+++ RScriptTarea.R
@@ -1,1 +1,5 @@
 #Corrige DataCamp / Nicolás Hellín Sánchez
+
+archivos = list.files("Entregas", recursive = T, full.names = T)
+
+puntos = archivos[grepl("puntos.*\\.txt$", archivos, ignore.case = T)]

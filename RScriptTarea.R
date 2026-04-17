b953e270caf021da51789fb4fdb1554140da68fc
nihesan <nihesan@alumni.uv.es>
2026-04-17 19:07:32 +0200

    Creado el algoritmo para encontrar apellidos y busqueda de certificados. La busqueda de certificados y de puntos necesita actualizarse ya que no encuentra a algunos alumnos debido a formatos incorrectos.

--- RScriptTarea.R
+++ RScriptTarea.R
@@ -2,4 +2,17 @@
 
 archivos = list.files("Entregas", recursive = T, full.names = T)
 
-puntos = archivos[grepl("puntos.*\\.txt$", archivos, ignore.case = T)]
+puntos = archivos[grepl("\\.txt$", archivos, ignore.case = T)]
+certificados = archivos[grepl("\\.(pdf|jpg|png)$", archivos, ignore.case = TRUE)]
+
+apellidos = c()
+
+for(i in 1:length(puntos)){
+  
+  partes = strsplit(puntos[i], "/")[[1]]
+  partes2 = partes[length(partes) - 1]
+  
+  apellido = strsplit(partes2, ",")[[1]][1]
+  
+  apellidos = c(apellidos, apellido)
+}

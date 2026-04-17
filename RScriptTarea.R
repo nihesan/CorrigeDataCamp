d35e1d8f4f89f4750594b64ffff1f7b58787f7f3
nihesan <nihesan@alumni.uv.es>
2026-04-17 19:27:41 +0200

    Mejorado el algoritmo de apellidos, ahora no depende de puntos, vector que es incorrecto debido a lo dicho antes.

--- RScriptTarea.R
+++ RScriptTarea.R
@@ -1,5 +1,6 @@
 #Corrige DataCamp / Nicolás Hellín Sánchez
 
+carpetas = list.files("Entregas", full.names=T)
 archivos = list.files("Entregas", recursive = T, full.names = T)
 
 puntos = archivos[grepl("\\.txt$", archivos, ignore.case = T)]
@@ -7,12 +8,13 @@ certificados = archivos[grepl("\\.(pdf|jpg|png)$", archivos, ignore.case = TRUE)
 
 apellidos = c()
 
-for(i in 1:length(puntos)){
+for(i in 1:length(carpetas)){
   
-  partes = strsplit(puntos[i], "/")[[1]]
-  partes2 = partes[length(partes) - 1]
+  partes = strsplit(carpetas[i], "/")[[1]]
   
-  apellido = strsplit(partes2, ",")[[1]][1]
+  parte2 = partes[length(partes)]
+  
+  apellido = strsplit(parte2, ",")[[1]][1]
   
   apellidos = c(apellidos, apellido)
 }

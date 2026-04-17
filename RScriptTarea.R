337a7bc4aefd7a752936d0dfcdfa76dcd9d2957e
nihesan <nihesan@alumni.uv.es>
2026-04-17 20:21:15 +0200

    Algoritmo eficiente(aunque mejorable, sigue devolviendo numeros incorrectos y NAs) para extraer las puntuaciones, contenido de los ficheros, los nombres de archivos(para ver cuantos distintos hay) y apellidos de cada nota(por los alumnos que no tienen fichero de puntos)

--- RScriptTarea.R
+++ RScriptTarea.R
@@ -18,3 +18,32 @@ for(i in 1:length(carpetas)){
   
   apellidos = c(apellidos, apellido)
 }
+
+puntuaciones = c()
+contenido_txt = c()
+nombres_archivo = c()
+apellidos_puntos = c() 
+
+for(i in 1:length(puntos)){
+  texto = readLines(puntos[i], warn = F)
+  linea = ifelse(length(texto) > 0, texto[1], "")
+  limpio = gsub("[^0-9,.]", "", linea)
+  limpio = gsub(",", ".", limpio)
+  if(nchar(limpio) > 4 && grepl("\\.", limpio)){
+    
+  limpio = gsub("\\.", "", limpio)}
+  
+  puntuaciones = c(puntuaciones, as.numeric(limpio))
+  
+  contenido_txt = c(contenido_txt, linea)
+  
+  partes = strsplit(puntos[i], "/")[[1]]
+  nombres_archivo = c(nombres_archivo, partes[length(partes)])
+  
+  carpeta_alumno = partes[2]
+  apellido = strsplit(carpeta_alumno, ",")[[1]][1]
+  apellidos_puntos = c(apellidos_puntos, apellido)
+}
+
+unique(nombres_archivo)
+#Hay un total de 16 nombres distintos para archivos de puntos.
